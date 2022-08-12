551a3fb79b42e825c26bef4e5d3bebb5ab056979
Mossa Merhi Reimert <cgmossa@gmail.com>
2022-08-12 11:41:18 +0200

    intervention should run daily

--- R/DiseaseFarm.R
+++ R/DiseaseFarm.R
@@ -2,7 +2,7 @@
 DiseaseFarm <- R6::R6Class(
   classname = "DiseaseFarm",
   public = list(
-    initialize = function(n_pigs, 
+    initialize = function(n_pigs,
                           rate_sus_to_expo = 0.400,
                           rate_expo_to_infect = (1 / 2.62),
                           rate_infect_to_recovered = 1 / 3.38,
@@ -15,6 +15,8 @@ DiseaseFarm <- R6::R6Class(
       private$intervention <- intervention
       #' all are susceptible in the beginning
       private$infected_status <- numeric(n_pigs)
+      private$d_infected_status <- numeric(n_pigs)
+      private$d_infected_status[] <- NA
       if (vaccination) {
         private$rate_sus_to_expo <- rate_sus_to_expo / 2.
       } else {
@@ -32,14 +34,16 @@ DiseaseFarm <- R6::R6Class(
         sample.int(private$n_pigs, size = total_infected, replace = FALSE)] = 1
     },
     update_daily = function() {
+      # ensures that disease status is overwritten by intervention
       self$update_disease_status();
-      # intervene on a weekly basis
-      if (private$intervention & (private$time_counter %% 7 == 0))  {
+      # intervene on a daily basis
+      if (private$intervention) {
         self$update_intervention();
-        
       }
+      private$apply_daily()
       private$time_counter <- private$time_counter + 1
     },
+    #' Internally updates the delta-updates. Remember to call `apply` after this.
     update_disease_status = function() {
       # how many susceptible become infected
       total_infected <- sum(private$infected_status == 1 |
@@ -58,20 +62,30 @@ DiseaseFarm <- R6::R6Class(
       
       
       # update state
-      private$infected_status[which_susceptible[sus_to_expo]] <- 1
-      private$infected_status[which_exposed[expo_to_infect]] <- 2
-      private$infected_status[which_infected[infected_to_recovered]] <- 3
+      private$d_infected_status[which_susceptible[sus_to_expo]] <- 1
+      private$d_infected_status[which_exposed[expo_to_infect]] <- 2
+      private$d_infected_status[which_infected[infected_to_recovered]] <- 3
       
     },
     
     #' Performs removal of infected animals (not exposed!)
+    #' Internally updates the delta-updates. 
+    #' Remember to call `apply` after this.
     update_intervention = function() {
       which_infectious <- which(private$infected_status == 2)
-      infectious_to_removal <- 
+      infectious_to_removal <-
         rbinom(length(which_infectious), size = 1, prob = private$rate_remove_clinical) == 1
       
-      private$infected_status[which_infectious[infectious_to_removal]] <- 3
+      private$d_infected_status[which_infectious[infectious_to_removal]] <- 3
       
+    },
+    #' must execute after the separate `update_*` routines
+    apply_daily = function() {
+      changed_status_mask <- !is.na(private$d_infected_status)
+      private$infected_status[changed_status_mask] <-
+        private$d_infected_status[changed_status_mask]
+      #' only revert the slots that was just used.. 
+      private$d_infected_status[changed_status_mask] <- NA
     },
     #' Reset the internal state variables for re-use in replications / repetitions.
     reset = function() {
@@ -82,7 +96,7 @@ DiseaseFarm <- R6::R6Class(
     },
     #' Returns current counts of the disease compartments
     disease_status = function() {
-      disease_status = list(
+      disease_status <- list(
         susceptible = sum(private$infected_status == 0),
         exposed = sum(private$infected_status == 1),
         infectious = sum(private$infected_status == 2),
@@ -101,6 +115,8 @@ DiseaseFarm <- R6::R6Class(
     rate_infect_to_recovered = numeric(0),
     rate_remove_clinical = numeric(0),
     infected_status = numeric(0),
+    #' delta changes on the disease states
+    d_infected_status = numeric(0),
     time_counter = integer(0),
     intervention = logical(0)
   )
